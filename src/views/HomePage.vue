648ed7805e1a449e6115ead0177d63685949b317
NISHAPURSWANI <2016.nisha.purswani@ves.ac.in>
2020-10-03 10:05:47 +0530

    Homepage and search nav bar

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,35 +9,35 @@
         </i>
         <i
           class="fas fa-home"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="fas fa-hashtag"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="far fa-bell"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="far fa-envelope"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="far fa-bookmark"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px; margin-left:2px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px; margin-left:2px;"
         ></i>
         <i
           class="far fa-list-alt"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="far fa-user"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <i
           class="fas fa-ellipsis-h"
-          style="font-size:50px; padding-top:10px; padding-bottom:10px;margin-top:10px;"
+          style="font-size:50px; padding-top:10px; padding-bottom:10px;"
         ></i>
         <div class="mytext">
           Home
@@ -51,6 +51,32 @@
         </div>
 
         <b-button pill variant="primary" class="twitter_button">Tweet</b-button>
+      </div>
+    </div>
+    <div class="navbar">
+      <div class="home">
+        <div class="hometext">
+        Home 
+        <span class="star"><i class="far fa-star" style="margin-top:8px;
+  margin-bottom: 14.1px; font-size:20px;"></i></span>
+        </div>
+        
+      </div>
+      <div class="searcharea">
+        <input type="search" placeholder="Search Twitter" class="searchbox"  >
+                 
+        </div>
+    </div>
+    <div class="contentarea">
+              <i class="far-fa user" style="height:49px; width:49px;"></i>
+
+      <div class="whatshappening">
+        <div class="whatshappeningcontent" >
+
+          <input type="text" placeholder="What's Happening?" class="whc">
+
+
+        </div>
       </div>
     </div>
   </div>
@@ -69,7 +95,7 @@
   margin-left: 30px;
   position: absolute;
   margin-left: 50px;
-  margin-top: 65px;
+  margin-top: 55px;
 }
 
 .leftside_nav {
@@ -115,6 +141,79 @@
   border: 0px;
 }
 .heading {
-  margin-top: 40px;
+  margin-top: 30px;
+}
+.navbar{
+  margin-left:300px;
+  height:auto;
+  width:auto;
+  border:1px solid black;
+  position:fixed;
+  background-color: white;
+}
+.home{
+  width:600px;
+  height:53px;
+  border:1px solid;
+  margin-left:-16px;
+}
+.hometext{
+  padding-top:0px;
+  padding-bottom: 0px; 
+  margin-top:11.1px;
+  margin-bottom: 14.1px;
+  margin-left: 15px;
+  font-size:20px;
+  font-weight: bold;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.star{
+  display:flex;
+  float:right;
+  margin-right: 15px;
+  
+}
+.searcharea{
+  margin-left:30px;
+}
+.searchbox{
+  padding:10px;
+  width:350px;
+  border-radius: 30px;
+  height:53px;
+  outline:none;
+  background-color: rgba(255,255,255,0.1);
+
+}
+.contentarea{
+margin-left:300px;
+  height:auto;
+  width:auto;
+  border:1px solid black;
+  position:fixed;
+  background-color: white;
+  margin-top:73px;
+
+}
+.whatshappening{
+   width:600px;
+  height:117px;
+  border:1px solid;
+
+
+}
+.whatshappeningcontent{
+  margin-left: 55px;
+  padding:10px;
+    font-size:20px;
+  font-weight: bold;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+.whc{
+   padding:10px;
+   height:48.94px;
+   width:505px;
+
 }
 </style>
